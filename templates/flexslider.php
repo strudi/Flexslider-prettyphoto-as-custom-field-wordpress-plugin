eb3912e0f7c81a5cdf4a82d5a70b90d70466acb4
Primoz <primozkrkovic@gmail.com>
2014-08-17 20:33:35 +0200

    flexslider template

--- templates/flexslider.php
+++ templates/flexslider.php
@@ -3,13 +3,12 @@
   ?>","animationLoop": true,"slideshowSpeed":<?php echo $this->plugin->get_option('img_interval__slide', 'cmb_slider_settings') ?>}'>
 	<ul class="slides">
 									
-		<?php foreach($this->plugin->get_attachment_ids()  as $attr_id) :  
+		<?php foreach($this->plugin->get_attachment_ids()  as $attr_id) :  			
 			
-			$src_attr = wp_get_attachment_image_src( $attr_id, $this->plugin->get_option('img_size_cmb_slide_large', 'cmb_slider_settings'));
 		   $attachment = wp_get_attachment( $attr_id );
 		    if ($src_attr): ?>			  
 				<li>
-					<img src="<?php echo $src_attr[0] ?>" alt="" />
+					<?php echo	wp_get_attachment_image( $attr_id ,$this->plugin->get_option('img_size_cmb_slide_large', 'cmb_slider_settings')); ?>
 					<?php if( $this->plugin->get_option('caption_cmb_slider', 'cmb_slider_settings') == 'yes'  ): ?>
 						<div class="flex-caption">
 							<h3><?php echo  $attachment->title ?></h3>
